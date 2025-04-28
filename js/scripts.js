0188d41ce6dab905b43ebbcbd733c9ce7e89a8e0
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-04-28 12:16:02 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -354,5 +354,28 @@ $(function() {
         }
         input.val(value + ' шт.').change(); // выводим полученное value в инпут; триггер .change() - на случай, если на изменение этого инпута у вас уже объявлен еще какой-то обработчик
     });
+    // let showCookie = getCookie('show_cookie');
+    // if(showCookie != 'no'){
+    
+        const cookieDialog = $('.cookie');
+            
+        setTimeout(function(){
+            cookieDialog.show();
+        }, 2000);
+    
+        const cookieAcceptBtn = $('.cookie__agree');
+        const cookieCloseBtn = $('.cookie__close');
+    
+        cookieCloseBtn.on('click', function(){
+            cookieDialog.hide();
+            // createCookie('show_cookie', 'no', '365');
+        });
+    
+        cookieAcceptBtn.on('click', function(){
+            cookieDialog.hide();
+            // createCookie('show_cookie', 'no', '365');
+        });
+    
+    // }
 
 });
